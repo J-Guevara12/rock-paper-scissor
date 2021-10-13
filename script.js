e1795222418c59d3c37269ab5327e4e44c1a1d38
Juan Esteban Guevara <juguevara@unal.edu.co>
2021-10-12 19:53:44 -0500

    Added UI

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function computerPlay() {
 
 function playRound(computer, player) {
 	if (computer == player) {return 'tie'}
-	return (player === 0 && computer === 2) ? true : (player > computer && player !== 2 && computer !== 0);
+	return (player === 0 && computer === 2) ? true : (player > computer && !(player == 2 && computer == 0));
 }
 function inputUser() {
 	let x = false;
@@ -20,6 +20,12 @@ function inputUser() {
 					false
 	}
 	return x;
+}
+
+function numbToLink(n) {
+	return (n == 0) ? 'img/rock.jpg' :
+		(n == 1) ? 'img/paper.jpg' :
+			'img/scissors.jpg';
 }
 
 function numbToStr(n) {
@@ -52,4 +58,46 @@ function play() {
 		count > 0 ? console.log('Th-th-that\'s impossible! a Human can\'t beat a machine!') :
 			console.log("Once again, the computer beat the useless human, Mr Anderson...")
 }
-play()
+function playButton(input) {
+	let machine = computerPlay();
+	userSelecion.src = numbToLink(input);
+	ourSelection.src = numbToLink(machine)
+	return playRound(machine, input)
+}
+
+function uploadScore(result) {
+	result == 'tie' ? userScoreCount -= 1 : null;
+	result ? userScoreCount += 1 : machineScoreCount += 1;
+	resultImage.src = result == 'tie' ? 'img/tie.jpg' : result ? 'img/win.jpg' : 'img/loose.jpg';
+	userScore.textContent = userScoreCount;
+	ourScore.textContent = machineScoreCount;
+}
+
+function playUser(input) {
+	let result = playButton(input);
+	uploadScore(result)
+}
+
+const userScore = document.querySelector('#player-score')
+const ourScore = document.querySelector('#machine-score')
+const userSelecion = document.querySelector('#player-selection')
+const ourSelection = document.querySelector('#machine-selection')
+const resultImage = document.querySelector('#result')
+
+const rockButton = document.querySelector('#op1');
+const paperButton = document.querySelector('#op2');
+const scissorsButton = document.querySelector('#op3')
+
+rockButton.addEventListener('click', () => {
+	playUser(0)
+})
+paperButton.addEventListener('click', () => {
+	playUser(1)
+})
+scissorsButton.addEventListener('click', () => {
+	playUser(2)
+})
+
+let userScoreCount = 0;
+let machineScoreCount = 0;
+
